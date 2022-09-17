13fed4364bb68781060ab8a9357f814a9e3368af
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-09-17 12:09:48 +0600

    Update ProductsController.php and show function implement for product get

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+    /**
+     * show
+     * @return array of product list.
+     */
+    function show(){
+        $products=Products::select('id','name','brand','model','price')->get();
+        return view('welcome',compact('products'));
+    }
 }
